8e905f7e77feca776427ff346b7ca095613db470
luisher98 <luisheratm@gmail.com>
2023-11-16 02:18:33 +0000

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import nodeExternals from "webpack-node-externals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "production",
+  entry: "./app.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "app.js",
+  },
+  target: "node",
+  externals: [nodeExternals()],
+};
